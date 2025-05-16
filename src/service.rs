da6810b402f07442ec0446b724a5fcb273124b41
Michael Dippery <michael@monkey-robot.com>
2025-05-16 10:32:05 -0400

    Spec out Service trait
    The Service trait defines some simple methods for making HTTP requests
    to the Reddit API.

--- src/service.rs
+++ src/service.rs
@@ -4,21 +4,46 @@
 //! with the Reddit API over HTTPS, essentially a specialized HTTPS client
 //! specifically for Reddit.
 
+type Uri = String; // TODO: Find a real type
+type RawResponse = &'static str; // TODO: Find a real type
+type JsonResponse = &'static str; // TODO: Find a real type
+
 /// A service for retrieving information for Reddit users.
 ///
 /// Using this trait, clients can implement different ways of connecting
 /// to the Reddit API, such as an actual connector for production code,
 /// and a mocked connector for testing purposes.
-pub trait Service {}
+pub trait Service {
+    /// Performs a GET request to the given URI and returns the raw body.
+    fn get(&self, uri: Uri) -> RawResponse;
+
+    /// Performs a GET request to the `resource` associated with the given
+    /// `username` and returns it as a parsed JSON response.
+    fn get_resource(&self, username: &str, resource: &str) -> JsonResponse;
+}
 
 /// A service that contacts the Reddit API directly to retrieve information.
 pub struct RedditService;
-
-impl Service for RedditService {}
 
 impl RedditService {
     /// Creates a new Reddit service.
     pub fn new() -> Self {
         Self {}
+    }
+}
+
+impl Service for RedditService {
+    fn get(&self, uri: Uri) -> RawResponse {
+        ""
+    }
+
+    fn get_resource(&self, username: &str, resource: &str) -> JsonResponse {
+        let qs = match resource {
+            "comments" => "?limit=100",
+            "submitted" => "?limit=100",
+            _ => "",
+        };
+        let uri = format!("https://www.reddit.com/user/{username}/{resource}.json{qs}");
+        self.get(uri)
     }
 }
