ed90f352d5beb06a0f862f16de320a824013a3d0
Michael Dippery <michael@monkey-robot.com>
2025-05-16 12:46:09 -0400

    Spec out parsing methods
    Eventually, these will take in a blob of JSON text and spit out a
    fully-formed Rust object...once I figure out how Serde works.

--- src/client.rs
+++ src/client.rs
@@ -1,21 +1,23 @@
 //! Clients for reading data from the Reddit API.
 
-use std::fmt;
 use crate::service::Service;
 use crate::thing::{Comment, DateTime, Submission, User};
+use std::fmt;
 
 /// Represents a Reddit user.
 pub struct Redditor<'a> {
-    /// Redditor's username
     username: &'a str,
-
-    /// A concrete connector for retrieving data from the Reddit API.
+    user: User,
     service: Box<dyn Service>,
 }
 
 impl<'a> fmt::Debug for Redditor<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Redditor {{ username = {} }}", self.username)
+        write!(
+            f,
+            "Redditor {{ username = {}, user = {:?} }}",
+            self.username, self.user
+        )
     }
 }
 
@@ -26,16 +28,17 @@ impl<'a> Redditor<'a> {
     /// actual service implementation that will be used to retrieve
     /// information about the Redditor.
     pub fn new(username: &'a str, service: Box<dyn Service>) -> Self {
-        Self { username, service }
-    }
-
-    fn user(&self) -> User {
-        User {}
+        let user = User::parse("", "", "").unwrap(); // TODO: Make HTTP calls and parse
+        Self {
+            username,
+            user,
+            service,
+        }
     }
 
     /// The date the account was created.
     pub fn created_at(&self) -> DateTime {
-        self.user().about().created_at()
+        self.user.about().created_at()
     }
 
     /// The age of the account, in seconds.
@@ -46,21 +49,21 @@ impl<'a> Redditor<'a> {
 
     /// Redditor's link karma
     pub fn link_karma(&self) -> u64 {
-        self.user().about().link_karma()
+        self.user.about().link_karma()
     }
 
     /// Redditor's comment karma
     pub fn comment_karma(&self) -> u64 {
-        self.user().about().comment_karma()
+        self.user.about().comment_karma()
     }
 
     /// Redditor's posts
-    pub fn submissions(&self) -> Vec<Submission> {
-        self.user().submissions()
+    pub fn submissions(&self) -> &Vec<Submission> {
+        self.user.submissions()
     }
 
     /// Redditor's comments
-    pub fn comments(&self) -> Vec<Comment> {
-        self.user().comments()
+    pub fn comments(&self) -> &Vec<Comment> {
+        self.user.comments()
     }
 }
--- src/thing.rs
+++ src/thing.rs
@@ -5,38 +5,68 @@
 //! module encapsulates that idea and provides an easy way to more or less
 //! work with JSON data from the Reddit API.
 
-pub type DateTime = u64;   // TODO: Find an appropriate DateTime type
+pub type DateTime = u64; // TODO: Find an appropriate DateTime type
 
 /// A Reddit user account.
-pub struct User;
+pub struct User {
+    about: About,
+    comments: Vec<Comment>,
+    submissions: Vec<Submission>,
+}
 
 /// Reddit user account data.
 pub struct About;
 
-/// A Reddit Post.
-pub struct Submission;
-
 /// A Reddit comment.
 pub struct Comment;
 
+/// A Reddit Post.
+pub struct Submission;
+
 impl User {
-    /// Returns account data for the user.
-    pub fn about(&self) -> About {
-        About {}
+    /// Parses text responses from the Reddit API into the associated
+    /// data structures.
+    ///
+    /// `user_data` is the result of a call to `/users/<user>/about.json`
+    /// and contains account medata, `comment_data` is the result of a call
+    /// to `/users/<user>/comments.json`, and `post_data` is the result of
+    /// a call to `/users/<user>/submitted.json`.
+    ///
+    /// Obviously parsing can fail so this method returns an `Option`.
+    pub fn parse(user_data: &str, comment_data: &str, post_data: &str) -> Option<Self> {
+        let about = About::parse(user_data)?;
+        let comments = Comment::parse(comment_data)?;
+        let submissions = Submission::parse(post_data)?;
+        Some(User { about, comments, submissions })
     }
 
-    /// User's submissions.
-    pub fn submissions(&self) -> Vec<Submission> {
-        Vec::new()
+    /// Returns account data for the user.
+    pub fn about(&self) -> &About {
+        &self.about
     }
 
     /// User's comments.
-    pub fn comments(&self) -> Vec<Comment> {
-        Vec::new()
+    pub fn comments(&self) -> &Vec<Comment> {
+        &self.comments
+    }
+
+    /// User's submissions.
+    pub fn submissions(&self) -> &Vec<Submission> {
+        &self.submissions
     }
 }
 
 impl About {
+    /// Parses a text response from the Reddit API into user data.
+    ///
+    /// Specifically, `user_data` is the result of a call to
+    /// `/users/<user>/about.json`.
+    ///
+    /// This method is generally invoked by `User`, not directly.
+    fn parse(user_data: &str) -> Option<Self> {
+        Some(About {})
+    }
+
     /// The date on which the account was created.
     pub fn created_at(&self) -> DateTime {
         0
@@ -50,5 +80,29 @@ impl About {
     /// User's current karma for comments.
     pub fn comment_karma(&self) -> u64 {
         0
+    }
+}
+
+impl Comment {
+    /// Parses a text response from the Reddit API into a list of comments.
+    ///
+    /// Specifically, `comment_data` is the result of a call to
+    /// `/users/<user>/comments.json`.
+    /// This method is generally invoked by `User`, not directly.
+    fn parse(comment_data: &str) -> Option<Vec<Self>> {
+        Some(vec![])
+    }
+}
+
+impl Submission {
+    /// Parses a text response from the Reddit API into a list of
+    /// submissions (posts).
+    ///
+    /// Specifically, `post_data` is the result of a call to
+    /// `/users/<user>/submitted.json`.
+    ///
+    /// This method is generally invoked by `User`, not directly.
+    fn parse(post_data: &str) -> Option<Vec<Self>> {
+        Some(vec![])
     }
 }
