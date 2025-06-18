d885af09056cf93f322d52d5c239de05ba45b601
Michael Dippery <michael@monkey-robot.com>
2025-06-18 15:17:48 -0700

    Reorganize Searchable tests under filter::tests::searchable
    Pretty soon there will be more structs and traits in this module that
    need to be tested.

--- src/filter.rs
+++ src/filter.rs
@@ -22,62 +22,64 @@ pub trait Searchable {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    mod searchable {
+        use super::super::*;
 
-    #[derive(Default, Debug)]
-    struct TestSearchable;
+        #[derive(Default, Debug)]
+        struct TestSearchable;
 
-    impl Searchable for TestSearchable {
-        fn search_text(&self) -> String {
-            String::from("peter piper picked a peck of pickled peppers")
+        impl Searchable for TestSearchable {
+            fn search_text(&self) -> String {
+                String::from("peter piper picked a peck of pickled peppers")
+            }
         }
-    }
 
-    #[test]
-    fn it_returns_true_if_there_is_a_match() {
-        let t = TestSearchable::default();
-        assert!(t.matches("peppers"));
-    }
+        #[test]
+        fn it_returns_true_if_there_is_a_match() {
+            let t = TestSearchable::default();
+            assert!(t.matches("peppers"));
+        }
 
-    #[test]
-    fn it_returns_true_if_there_are_multiple_matches() {
-        let t = TestSearchable::default();
-        assert!(t.matches("pick"));
-    }
+        #[test]
+        fn it_returns_true_if_there_are_multiple_matches() {
+            let t = TestSearchable::default();
+            assert!(t.matches("pick"));
+        }
 
-    #[test]
-    fn it_matches_substrings() {
-        let t = TestSearchable::default();
-        assert!(t.matches("pip"));
-    }
+        #[test]
+        fn it_matches_substrings() {
+            let t = TestSearchable::default();
+            assert!(t.matches("pip"));
+        }
 
-    #[test]
-    fn it_matches_needles_with_spaces() {
-        let t = TestSearchable::default();
-        assert!(t.matches("picked a peck"));
-    }
+        #[test]
+        fn it_matches_needles_with_spaces() {
+            let t = TestSearchable::default();
+            assert!(t.matches("picked a peck"));
+        }
 
-    #[test]
-    fn it_returns_false_if_there_are_no_matches() {
-        let t = TestSearchable::default();
-        assert!(!t.matches("usaidwait"));
-    }
+        #[test]
+        fn it_returns_false_if_there_are_no_matches() {
+            let t = TestSearchable::default();
+            assert!(!t.matches("usaidwait"));
+        }
 
-    #[test]
-    fn it_matches_regexes() {
-        let t = TestSearchable::default();
-        assert!(t.matches("pep{2,}ers"));
-    }
+        #[test]
+        fn it_matches_regexes() {
+            let t = TestSearchable::default();
+            assert!(t.matches("pep{2,}ers"));
+        }
 
-    #[test]
-    fn it_matches_regexes_case_insensitively() {
-        let t = TestSearchable::default();
-        assert!(t.matches("Piper"));
-    }
+        #[test]
+        fn it_matches_regexes_case_insensitively() {
+            let t = TestSearchable::default();
+            assert!(t.matches("Piper"));
+        }
 
-    #[test]
-    fn it_treats_invalid_regexes_as_a_fixed_string() {
-        let t = TestSearchable::default();
-        assert!(!t.matches("pic{?}kl**ed"));
+        #[test]
+        fn it_treats_invalid_regexes_as_a_fixed_string() {
+            let t = TestSearchable::default();
+            assert!(!t.matches("pic{?}kl**ed"));
+        }
     }
 }
