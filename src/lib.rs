70f0c9987a6e98bca872a26b3c125549eb50cc6d
Michael Dippery <michael@monkey-robot.com>
2025-07-30 10:58:24 -0700

    Expand OpenAI API setup documentation
    I guess you can never stress this enough; who knows what people will
    actually read.

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,13 @@
 //! usaidwat log reddit_user
 //! ```
 //!
+//! Summarize a user's last 100 comments and provide a tone and sentiment analysis
+//! using AI:
+//!
+//! ```bash
+//! usaidwat summary reddit_user
+//! ```
+//!
 //! Show a count of the user's last 100 comments by subreddit:
 //!
 //! ```bash
@@ -68,13 +75,47 @@
 //! usaidwat --help
 //! ```
 //!
+//! # OpenAI API Setup
+//!
+//! To use the summarization feature provided by `usaidwat summary` and the
+//! [`Summarizer`](summary::Summarizer), you must set up access to OpenAI.
+//! To enable access:
+//!
+//! 1. Set up an [OpenAI API account].
+//! 2. Generate an [API key].
+//! 3. Copy and paste the generated key.
+//! 4. Store the generated key in your shell's `$OPENAI_API_KEY` environment
+//!    variable. Follow your shell's procedure for configuring environment
+//!    variables, but generally this involves running
+//!
+//!    ```bash
+//!    $ export OPENAI_API_KEY='copied api key'
+//!    ```
+//!
+//!    In your shell session or in your shell's configuration ("rc") file
+//!    (e.g., `~/.bashrc` or `~/.zshrc`).
+//!
+//! **You are solely responsible for the cost of your use of the OpenAI API!**
+//! See the [openai module documentation] for more information on the cost of
+//! using the OpenAI API.
+//!
+//! By default, `usaidwat summary` will use the [cheapest model]; see
+//! `usaidwat summary -h` for other options.
+//!
+//! Currently only OpenAI's API is supported by usaidwat, but support for additional
+//! providers may be added in the future.
+//!
 //! # License
 //!
 //! usaidwat is licensed under the terms of the [Apache License 2.0]. Please
-//! see the LICENSE file accompanying this source code for visit the previous
+//! see the LICENSE file accompanying this source code or visit the previous
 //! link for more information on licensing.
 //!
 //! [Apache License 2.0]: https://www.apache.org/licenses/LICENSE-2.0
+//! [API key]: https://platform.openai.com/settings/organization/api-keys
+//! [OpenAI API account]: https://platform.openai.com/docs/overview
+//! [cheapest model]: crate::ai::client::AIModel::cheapest()
+//! [openai module documentation]: crate::ai::client::openai
 
 pub mod ai;
 pub mod cli;
