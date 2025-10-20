4c7651292aaefb5efcc6db09757bb37acc7cd7a5
Michael Dippery <michael@monkey-robot.com>
2025-10-20 15:26:26 -0700

    Expand documentation
    This is mostly for preparation for refactoring this module into a new
    crate, but it's nice to have regardless.

--- src/ai/service.rs
+++ src/ai/service.rs
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2025 Michael Dippery <michael@monkey-robot.com>
 
-//! Services for communicating with APIs using HTTP.
+//! Services for communicating with AI service providers over HTTP.
+//!
+//! [`Service`] acts as a proxy for an AI service provider's API (often
+//! implemented as a REST API). As an implementation-agnostic definition
+//! of a service, it allows consumers to implement a single API client
+//! that can communicate with the AI using various mechanisms. In
+//! particularly, it provides an easy way to "mock" an API client's
+//! HTTP functionality in testing by providing a mocked `Service`
+//! implementation for an API client under test, or an actual HTTP
+//! client when the API client is used in production.
+//!
+//! # See Also
+//!
+//! - [`hypertyper.service`] for an example of how to use `Service` to mock
+//!   HTTP calls.
+//!
+//! [`hypertyper.service`]: https://docs.rs/hypertyper/latest/hypertyper/service/index.html
 
-use hypertyper::{Auth, HTTPClient, HTTPClientFactory, HTTPResult, HTTPService};
-use reqwest::{IntoUrl, header};
+use hypertyper::{Auth, HTTPClient, HTTPClientFactory, HTTPResult, HTTPService, IntoUrl};
+use reqwest::header;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 
@@ -13,14 +29,15 @@ use serde::de::DeserializeOwned;
 /// This is the "default" service used by most AI API clients. It more or
 /// less just wraps a Reqwest client, making it easier to swap out the
 /// service for a deterministic service when writing tests. Most AI API
-/// clients should use this `APIService` by default.
+/// clients should use this `Service` by default.
 #[derive(Debug)]
 pub struct Service {
     client: HTTPClient,
 }
 
 impl Service {
-    /// Creates a new HTTP service using clients from the given factory.
+    /// Creates a new HTTP service that communicate using clients from the
+    /// given factory.
     pub fn new(factory: HTTPClientFactory) -> Self {
         let client = factory.create();
         Self { client }
